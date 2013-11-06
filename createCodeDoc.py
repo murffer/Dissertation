41b130e3e12dfca47245d42fc92677cc943e4166
Matthew J. Urffer <matthew.urffer@gmail.com>
2013-11-06 15:57:29 -0500

    Added the code documentation
    Added the README of the code documentation. I will add a section on the
    getting the code from github

--- createCodeDoc.py
+++ createCodeDoc.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import os
-import fnmatch
 import subprocess
 
 def convertToLatex(markdownFile):
@@ -10,20 +9,55 @@ def convertToLatex(markdownFile):
     """
     drive, path = os.path.splitdrive(markdownFile)
     path, filename = os.path.split(path)
-    filename, ext = os.splitext(filename)
-    outFile = os.path.join(path,filename+'.tex')
-    print 'In file: {} outfile: {}'.format(markdownFile,outFile)
+    filename, ext = os.path.splitext(filename)
+    outDir = 'doc/'
+    outFile = os.path.join(outDir+os.path.basename(path)+'.tex')
     cmd = 'pandoc -f markdown -t latex {} > {}'.format(markdownFile,outFile)
-    #subprocess.call(cmd,shell=True)
+    subprocess.call(cmd,shell=True)
+    #with open(outFile,'a') as out:
+    #    out.write(createListingsString(path))
+    return os.path.basename(path)
 
 def getAllFiles(directory):
+    """
+    Gets all of the files (recursivley) in a directory
+    """
     file_paths = list()
     for root, directories, files in os.walk(directory):
         for filename in files:
             filepath = os.path.join(root, filename)
             file_paths.append(filepath)
-    print file_paths
     return file_paths       
+
+def getSourceFiles(path):
+    """
+    Gets the source, header, macros and run files of a directory
+    """
+    matches = set()
+    print 'The source files path is '+str(path)
+    files = getAllFiles(path)
+    print 'The files found are:'
+    print files
+    for f in files:
+        if f.endswith(('.mac','.py','.cc','.hh','.mcnp')):
+            matches.add(f)
+    print matches
+    return matches
+    
+def createListingsString(path):
+    srcs = getSourceFiles(path)
+    fmtString = '\lstinputlisting[language={}]{{{}}}\n'
+    s = ''
+    for src in srcs:
+        if src.endswith('.py'):
+            language='Python'
+        elif src.endswith(('.cc','.hh')):
+            language='C++'
+        else:
+            language='bash'
+        s += fmtString.format(language,src)
+    return s
+            
 
 def getListOfFiles(directory):
     matches = list()
@@ -34,7 +68,11 @@ def getListOfFiles(directory):
     return matches
         
 if __name__ == '__main__':
-    files = getListOfFiles('/Users/murffer/Documents/Dissertation/code')
-    print files
+    files = getListOfFiles('code')
+    toWrite = set()
     for f in files:
-        convertToLatex(f)
+        flatex = convertToLatex(f)
+        toWrite.add(flatex)
+    with open('doc/CodeDoc.tex','w') as out:
+        for f in toWrite:
+            out.write('\input{doc/'+f+'}\n')
